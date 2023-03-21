ee3648fcb44497238aeb6eb8c2d098f18f533174
TommasoTarchi <tommaso.tarchi@gmail.com>
2023-03-21 16:40:36 +0100

    added working serial oredered evolution

--- exercise1/serial_gol.c
+++ exercise1/serial_gol.c
@@ -186,27 +186,101 @@ srand(time(NULL));
 
 
 
+    /////// valutare se nei confronti degli if statement non convenga usare bitwise operations
 
 
-
+        /* ordered evolution */
         if (e == ORDERED) {
+
 
             for (int gen=0; gen<n; gen++) {
 
-                /* iteration on rows */
+
+                /* updating first row */
+                
+                /* first element of the first row */
+                short int count = 0;
+                if (grid[1] == '1')
+                    count++;
+                for (int b=k; b<k+2; b++) {
+                    if (grid[b] == '1')
+                        count ++;
+                }
+
+                if (count == 2 || count == 3) {
+                    grid[0] = '1';
+                } else {
+                    grid[0] = '0';
+                }
+
+                /* internal elements of the first row */
+                for (int j=1; j<k-1; j++) {
+
+                    count = 0;
+                    for (int a=0; a<2; a++) {
+                        for (int b=j-1; b<j+2; b++) {
+                            if (grid[a*k+b] == '1')
+                                count++;
+                        }
+                    }
+                    if (grid[j] == '1')
+                        count--;
+
+                    if (count == 2 || count == 3) {
+                        grid[j] = '1';
+                    } else {
+                        grid[j] = '0';
+                    }
+                }
+
+                /* last element of the first row */
+                count = 0;
+                if (grid[k-2] == '1')
+                    count++;
+                for (int b=2*k-2; b<2*k; b++) {
+                    if (grid[b] == '1')
+                        count++;
+                }
+
+                if (count == 2 || count == 3) {
+                    grid[k-1] = '1';
+                } else {
+                    grid[k-1] = '0';
+                }
+
+
+                /* updating internal cells */ 
+
+                /* iteration on internal rows */
                 for (int i=1; i<k-1; i++) {
-                    /* iteration on columns */
+
+                    /* first element of the row */
+                    count = 0;
+                    for (int a=i-1; a<i+2; a++) {
+                        for (int b=0; b<2; b++) {
+                            if (grid[a*k+b] == '1')
+                                count++;
+                        }
+                    }
+                    if (grid[i*k] == '1')
+                        count--;
+
+                    if (count == 2 || count == 3) {
+                        grid[i*k] = '1';
+                    } else {
+                        grid[i*k] = '0';
+                    }
+
+                    /* iteration on internal columns */
                     for (int j=1; j<k-1; j++) {
 
-                        short int count = 0;
+                        count = 0;
                         for (int a=i-1; a<i+2; a++) {
                             for (int b=j-1; b<j+2; b++) {
-                                if (grid[a*k+b] == '1') {
+                                if (grid[a*k+b] == '1')
                                     count++;
-                                }
                             }
                         }
-
                         if (grid[i*k+j] == '1')
                             count--;
 
@@ -216,10 +290,85 @@ srand(time(NULL));
                             grid[i*k+j] = '0';
                         }
                     }
+
+                    /* last element of the row */
+                    count = 0;
+                    for (int a=i; a<i+3; a++) {
+                        for (int b=-2; b<0; b++) {
+                            if (grid[a*k+b] == '1')
+                                count++;
+                        }
+                    }
+                    if (grid[(i+1)*k-1] == '1')
+                        count--;
+
+                    if (count == 2 || count == 3) {
+                        grid[(i+1)*k-1] = '1';
+                    } else {
+                        grid[(i+1)*k-1] = '0';
+                    }
                 }
+                
+
+                /* updating last row */
+                
+                /* first element of the last row */
+                count = 0;
+                for (int b=(k-2)*k; b<(k-2)*k+2; b++) {
+                    if (grid[b] == '1')
+                        count++;
+                }
+                if (grid[n_cells-k+1] == '1')
+                    count++;
+                
+                if (count == 2 || count == 3) {
+                    grid[n_cells-k] = '1';
+                } else {
+                    grid[n_cells-k] = '0';
+                }
+
+                /* internal elements of the last row */
+                for (int j=1; j<k-1; j++) {
+
+                    count = 0;
+                    for (int a=k-2; a<k; a++) {
+                        for (int b=j-1; b<j+2; b++) {
+                            if (grid[a*k+b] == '1')
+                                count++;
+                        }
+                    }
+                    if (grid[n_cells-k+j] == '1')
+                        count--;
+
+                    if (count == 2 || count == 3) {
+                        grid[n_cells-k+j] = '1';
+                    } else {
+                        grid[n_cells-k+j] = '0';
+                    }
+                }
+
+                /* last element of the last row */
+                count = 0;
+                for (int b=n_cells-k-2; b<n_cells-k; b++) {
+                    if (grid[b] == '1')
+                        count++;
+                }
+                if (grid[n_cells-2] == '1')
+                    count++;
+
+                if (count == 2 || count == 3) {
+                    grid[n_cells-1] = '1';
+                } else {
+                    grid[n_cells-1] = '0';
+                }
+
+
+                // aggiungere playground dumping
 
             }
 
+
+        /* static evolution */
         } else if (e == STATIC) {
 
             printf("static\n");
