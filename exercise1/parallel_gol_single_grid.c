cf8832d2c9d9dc9f2150ba0ec7f16b5f4fec31e0
TommasoTarchi <tommaso.tarchi@gmail.com>
2023-04-01 10:30:54 +0200

    fixed issue with bit_control

--- exercise1/parallel_gol_single_grid.c
+++ exercise1/parallel_gol_single_grid.c
@@ -636,7 +636,7 @@ int main(int argc, char **argv) {
             for (int gen=0; gen<n; gen++) {
 
 
-		/* writing a dump of the system */ 
+		        /* writing a dump of the system */ 
 
                 if (s != 0) {
 
@@ -646,14 +646,15 @@ int main(int argc, char **argv) {
                         /* selecting the state signaling bit */
                         if (bit_control % 2 == 1) {
 
-                            for (int i=0; i<my_n_cells; i++)
+                            for (int i=0; i<my_n_cells+2*x_size; i++)
                                 my_grid[i] >>= 1;
+                            bit_control++
                         
                         } else {
 
-                            for (int i=0; i<my_n_cells; i++)
+                            for (int i=0; i<my_n_cells+2*x_size; i++)
                                 my_grid[i] &= 1;
-			}
+			            }
 
 
                         sprintf(snap_name, "snapshots/snapshot_%05d.pgm", gen);
