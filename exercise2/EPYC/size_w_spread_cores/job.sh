f094880de5dd425112436c760820b99454c5da84
TommasoTarchi <tommaso.tarchi@gmail.com>
2023-04-02 17:30:57 +0200

    gathered results on ORFEO

--- exercise2/EPYC/size_w_spread_cores/job.sh
+++ exercise2/EPYC/size_w_spread_cores/job.sh
@@ -42,65 +42,71 @@ export OMP_PROC_BIND=$alloc
 export OMP_NUM_THREADS=$ncores
 
 ### overwriting old datafiles and setting up new ones
-echo "#node:        ${node}" > mkl_f.csv
-echo "#library:     MKL" >> mkl_f.csv
-echo "#precision:   float" >> mkl_f.csv
-echo "#allocation:  ${alloc}" >> mkl_f.csv
-echo >> mkl_f.csv
-echo "n_cores,mat_size,time(s),GFLOPS" >> mkl_f.csv
+#echo "#,,," > mkl_f.csv
+#echo "#node:,${node},," >> mkl_f.csv
+#echo "#library:,MKL,," >> mkl_f.csv
+#echo "#precision:,float,," >> mkl_f.csv
+#echo "#allocation:,${alloc},," >> mkl_f.csv
+#echo "#,,," >> mkl_f.csv
+#echo "cores,mat_size,time(s),GFLOPS" >> mkl_f.csv
 
-###echo "#node:        ${node}" > oblas_f.csv
-###echo "#library:     openBLAS" >> oblas_f.csv
-###echo "#precision:   float" >> oblas_f.csv
-###echo "#allocation:  ${alloc}" >> oblas_f.csv
-###echo >> oblas_f.csv
-###echo "cores,mat_size,time(s),GFLOPS" >> oblas_f.csv
+#echo "#,,," > oblas_f.csv
+#echo "#node:,${node},," >> oblas_f.csv
+#echo "#library:,openBLAS,," >> oblas_f.csv
+#echo "#precision:,float,," >> oblas_f.csv
+#echo "#allocation:,${alloc},," >> oblas_f.csv
+#echo "#,,," >> oblas_f.csv
+#echo "cores,mat_size,time(s),GFLOPS" >> oblas_f.csv
 
-###echo "#node:        ${node}" > blis_f.csv
-###echo "#library:     BLIS" >> blis_f.csv
-###echo "#precision:   float" >> blis_f.csv
-###echo "#allocation:  ${alloc}" >> blis_f.csv
-###echo >> blis_f.csv
-###echo "cores,mat_size,time(s),GFLOPS" >> blis_f.csv
+#echo "#,,," > blis_f.csv
+#echo "#node:,${node},," >> blis_f.csv
+#echo "#library:,BLIS,," >> blis_f.csv
+#echo "#precision:,float,," >> blis_f.csv
+#echo "#allocation:,${alloc},," >> blis_f.csv
+#echo "#,,," >> blis_f.csv
+#echo "cores,mat_size,time(s),GFLOPS" >> blis_f.csv
 
-###echo "#node:        ${node}" > mkl_d.csv
-###echo "#library:     MKL" >> mkl_d.csv
-###echo "#precision:   double" >> mkl_d.csv
-###echo "#allocation:  ${alloc}" >> mkl_d.csv
-###echo >> mkl_d.csv
-###echo "cores,mat_size,time(s),GFLOPS" >> mkl_d.csv
+#echo "#,,," > mkl_d.csv
+#echo "#node:,${node},," >> mkl_d.csv
+#echo "#library:,MKL,," >> mkl_d.csv
+#echo "#precision:,double,," >> mkl_d.csv
+#echo "#allocation:,${alloc},," >> mkl_d.csv
+#echo "#,,," >> mkl_d.csv
+#echo "cores,mat_size,time(s),GFLOPS" >> mkl_d.csv
 
-###echo "#node:        ${node}" > oblas_d.csv
-###echo "#library:     openBLAS" >> oblas_d.csv
-###echo "#precision:   double" >> oblas_d.csv
-###echo "#allocation:  ${alloc}" >> oblas_d.csv
-###echo >> oblas_d.csv
-###echo "cores,mat_size,time(s),GFLOPS" >> oblas_d.csv
+#echo "#,,," > oblas_d.csv
+#echo "#node:,${node},," >> oblas_d.csv
+#echo "#library:,openBLAS,," >> oblas_d.csv
+#echo "#precision:,double,," >> oblas_d.csv
+#echo "#allocation:,${alloc},," >> oblas_d.csv
+#echo "#,,," >> oblas_d.csv
+#echo "cores,mat_size,time(s),GFLOPS" >> oblas_d.csv
 
-###echo "#node:        ${node}" > blis_d.csv
-###echo "#library:     BLIS" >> blis_d.csv
-###echo "#precision:   double" >> blis_d.csv
-###echo "#allocation:  ${alloc}" >> blis_d.csv
-###echo >> blis_d.csv
-###echo "cores,mat_size,time(s),GFLOPS" >> blis_d.csv
+#echo "#,,," > blis_d.csv
+#echo "#node:,${node},," >> blis_d.csv
+#echo "#library:,BLIS,," >> blis_d.csv
+#echo "#precision:,double,," >> blis_d.csv
+#echo "#allocation:,${alloc},," >> blis_d.csv
+#echo "#,,," >> blis_d.csv
+#echo "cores,mat_size,time(s),GFLOPS" >> blis_d.csv
 
 ### performing measures
 for size in $(seq 2000 250 20000)
 do
 	for count in $(seq 1 1 5)
 	do
-		###echo -n "${ncores}," >> mkl_f.csv
-		###./gemm_mkl_f.x $size $size $size
-		###echo -n "${ncores},"  >> oblas_f.csv
-		###./gemm_oblas_f.x $size $size $size
-		###echo -n "${ncores},"  >> blis_f.csv
-		###./gemm_blis_f.x $size $size $size
-		###echo -n "${ncores}," >> mkl_d.csv
-		###./gemm_mkl_d.x $size $size $size
-		###echo -n "${ncores},"  >> oblas_d.csv
-		###./gemm_oblas_d.x $size $size $size
-		echo -n "${ncores},"  >> blis_d.csv
-		./gemm_blis_d.x $size $size $size
+		#echo -n "${ncores}," >> mkl_f.csv
+		#./gemm_mkl_f.x $size $size $size
+		#echo -n "${ncores},"  >> oblas_f.csv
+		#./gemm_oblas_f.x $size $size $size
+		#echo -n "${ncores},"  >> blis_f.csv
+		#./gemm_blis_f.x $size $size $size
+		#echo -n "${ncores}," >> mkl_d.csv
+		#./gemm_mkl_d.x $size $size $size
+		#echo -n "${ncores},"  >> oblas_d.csv
+		#./gemm_oblas_d.x $size $size $size
+		#echo -n "${ncores},"  >> blis_d.csv
+		#./gemm_blis_d.x $size $size $size
 		echo
 		echo -----------
 		echo
