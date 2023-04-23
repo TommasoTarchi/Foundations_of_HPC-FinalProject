1c6ef44dca15e18ca432adb538a0d434d8a21b4f
TommasoTarchi <tommaso.tarchi@gmail.com>
2023-04-23 16:11:27 +0200

    merged serial gol and serial gol with single grid

--- exercise1/serial_gol.c
+++ exercise1/serial_gol.c
@@ -22,6 +22,7 @@
 
 #define ORDERED 0
 #define STATIC  1
+#define STATIC_SAVE_MEM 2
 
 #define BOOL char
 
@@ -37,7 +38,7 @@ char *fname  = NULL;
 
 
 
-void write_pgm_image(BOOL*, const int, int, int, const char*); 
+void write_pgm_image(BOOL*, const int, int, int, const char*, int);
 void read_pgm_image(BOOL**, int*, int*, int*, const char*);
 
 
@@ -107,7 +108,6 @@ int main(int argc, char **argv) {
     BOOL* grid = NULL;
 
 
-
     /* initializing a playground */
     if (action == INIT) {
 
@@ -137,7 +137,7 @@ int main(int argc, char **argv) {
 
         /* writing down playground */
         const int maxval_color = 1;
-        write_pgm_image(grid, maxval_color, k, m, fname);
+        write_pgm_image(grid, maxval_color, k, m, fname, 1);
 
     }
 
@@ -200,7 +200,6 @@ int main(int argc, char **argv) {
         ///////////////////////////////////////////////
 
 
-
         
         /* string to store the name of the snapshot files */
         char* snap_name = (char*) malloc(29*sizeof(char));
@@ -209,6 +208,9 @@ int main(int argc, char **argv) {
 
         /* ordered evolution */
         if (e == ORDERED) {
+
+
+            printf("using ordered evolution\n\n");
 
 
 #ifdef TIME
@@ -234,7 +236,7 @@ int main(int argc, char **argv) {
                 }
                 count += grid[n_cells-1];
 
-                if (count > 1 && count < 4) {
+                if (count == 2 || count == 3) {
                     grid[0] = 1;
                 } else {
                     grid[0] = 0;
@@ -253,7 +255,7 @@ int main(int argc, char **argv) {
                         count += grid[b];
                     }
 
-                    if (count > 1 && count < 4) {
+                    if (count == 2 || count == 3) {
                         grid[j] = 1;
                     } else {
                         grid[j] = 0;
@@ -273,7 +275,7 @@ int main(int argc, char **argv) {
                     count += grid[b];
                 }
 
-                if (count > 1 && count < 4) {
+                if (count == 2 || count == 3) {
                     grid[x_size-1] = 1;
                 } else {
                     grid[x_size-1] = 0;
@@ -296,8 +298,10 @@ int main(int argc, char **argv) {
                         count += grid[b];
                     }
                     count += grid[(i+2)*x_size-1]; 
+                    if (grid[i*x_size] == 1)
+                        count--;
 
-                    if (count > 1 && count < 4) {
+                    if (count == 2 || count == 3) {
                         grid[i*x_size] = 1;
                     } else {
                         grid[i*x_size] = 0;
@@ -316,7 +320,7 @@ int main(int argc, char **argv) {
                             count += grid[b];
                         }
 
-                        if (count > 1 && count < 4) {
+                        if (count == 2 || count == 3) {
                             grid[i*x_size+j] = 1;
                         } else {
                             grid[i*x_size+j] = 0;
@@ -335,7 +339,7 @@ int main(int argc, char **argv) {
                         count += grid[b];
                     }
 
-                    if (count > 1 && count < 4) {
+                    if (count == 2 || count == 3) {
                         grid[(i+1)*x_size-1] = 1;
                     } else {
                         grid[(i+1)*x_size-1] = 0;
@@ -358,7 +362,7 @@ int main(int argc, char **argv) {
                 }
                 count += grid[x_size-1];
 
-                if (count > 1 && count < 4) {
+                if (count == 2 || count == 3) {
                     grid[n_cells-x_size] = 1;
                 } else {
                     grid[n_cells-x_size] = 0;
@@ -377,7 +381,7 @@ int main(int argc, char **argv) {
                         count += grid[b];
                     }
 
-                    if (count > 1 && count < 4) {
+                    if (count == 2 || count == 3) {
                         grid[n_cells-x_size+j] = 1;
                     } else {
                         grid[n_cells-x_size+j] = 0;
@@ -396,7 +400,7 @@ int main(int argc, char **argv) {
                     count += grid[b];
                 }
 
-                if (count > 1 && count < 4) {
+                if (count == 2 || count == 3) {
                     grid[n_cells-1] = 1;
                 } else {
                     grid[n_cells-1] = 0;
@@ -410,7 +414,7 @@ int main(int argc, char **argv) {
 
                         sprintf(snap_name, "snapshots/snapshot_%05d.pgm", gen+1);
                         
-                        write_pgm_image(grid, 1, x_size, y_size, snap_name);
+                        write_pgm_image(grid, 1, x_size, y_size, snap_name, 1);
                     }
                 }
                 
@@ -423,8 +427,12 @@ int main(int argc, char **argv) {
 #endif
 
 
+
         /* static evolution */
         } else if (e == STATIC) {
+
+
+            printf("using static evolution\n\n");
 
 
 #ifdef TIME
@@ -440,19 +448,7 @@ int main(int argc, char **argv) {
 
             for (int gen=0; gen<n; gen++) {
 
-
-                /* saving a snapshot of the system */
-                if (s != 0) {
-
-                    if (gen % s == 0) {
-
-                        sprintf(snap_name, "snapshots/snapshot_%05d.pgm", gen);
-                        
-                        write_pgm_image(grid, 1, x_size, y_size, snap_name);
-                    }
-                }
-
-                
+               
                 /* updating first row */
                 
                 /* first element of the first row */
@@ -642,10 +638,250 @@ int main(int argc, char **argv) {
                 grid = grid_aux;
                 grid_aux = temp;
                 temp = NULL;
+
+
+                /* saving a snapshot of the system */
+                if (s != 0) {
+
+                    if (gen % s == 0) {
+
+                        sprintf(snap_name, "snapshots/snapshot_%05d.pgm", gen+1);
+                        
+                        write_pgm_image(grid, 1, x_size, y_size, snap_name, 1);
+                    }
+                }
+
                 
             }
 
             free(grid_aux);
+
+
+#ifdef TIME 
+    double time = CPU_TIME - t_start;
+    printf("elapsed time: %f sec\n\n", time);
+#endif
+
+
+
+        /* static evolution with single grid */
+        } else if (e == STATIC_SAVE_MEM) {
+
+
+            printf("using static evolution with a single grid in memory\n\n");
+
+
+#ifdef TIME
+    double t_start = CPU_TIME;
+#endif
+
+
+
+            for (int gen=0; gen<n; gen++) {
+
+
+                /* needed for evolution without allocating auxiliary grid */
+                char alive = 2 - gen % 2;
+                char dead = 1 + gen % 2;
+                char shift = gen % 2;
+
+
+                /* updating first row */
+                
+                /* first element of the first row */
+                char count = 0;
+
+                count += (grid[1] >> shift) & 1;
+                for (int b=x_size-1; b<x_size+2; b++) {
+                    count += (grid[b] >> shift) & 1;
+                }
+                count += (grid[2*x_size-1] >> shift) & 1;
+                for (int b=n_cells-x_size; b<n_cells-x_size+2; b++) {
+                    count += (grid[b] >> shift) & 1;
+                }
+                count += (grid[n_cells-1] >> shift) & 1;
+
+                if (count == 2 || count == 3) {
+                    grid[0] |= alive;
+                } else {
+                    grid[0] &= dead;
+                }
+
+                /* internal elements of the first row */
+                for (int j=1; j<x_size-1; j++) {
+
+                    count = 0;
+                    count += (grid[j-1] >> shift) & 1;
+                    count += (grid[j+1] >> shift) & 1;
+                    for (int b=j-1; b<j+2; b++) {
+                        count += (grid[x_size+b] >> shift) & 1;
+                    }
+                    for (int b=n_cells-x_size+j-1; b<n_cells-x_size+j+2; b++) {
+                        count += (grid[b] >> shift) & 1;
+                    }
+
+                    if (count == 2 || count == 3) {
+                        grid[j] |= alive;
+                    } else {
+                        grid[j] &= dead;
+                    }
+                }
+
+                /* last element of the first row */
+                count = 0;
+                count += (grid[0] >> shift) & 1;
+                count += (grid[x_size-2] >> shift) & 1;
+                count += (grid[x_size] >> shift) & 1;
+                for (int b=2*x_size-2; b<2*x_size; b++) {
+                    count += (grid[b] >> shift) & 1;
+                }
+                count += (grid[n_cells-x_size] >> shift) & 1;
+                for (int b=n_cells-2; b<n_cells; b++) {
+                    count += (grid[b] >> shift) & 1;
+                }
+
+                if (count == 2 || count == 3) {
+                    grid[x_size-1] |= alive;
+                } else {
+                    grid[x_size-1] &= dead;
+                }
+
+
+                /* updating internal cells */ 
+
+                /* iteration on internal rows */
+                for (int i=1; i<y_size-1; i++) {
+
+                    /* first element of the row */
+                    count = 0;
+                    for (int b=(i-1)*x_size; b<(i-1)*x_size+2; b++) {
+                        count += (grid[b] >> shift) & 1;
+                    }
+                    count += (grid[i*x_size-1] >> shift) & 1;
+                    count += (grid[i*x_size+1] >> shift) & 1;
+                    for (int b=(i+1)*x_size-1; b<(i+1)*x_size+2; b++) {
+                        count += (grid[b] >> shift) & 1;
+                    }
+                    count += (grid[(i+2)*x_size-1] >> shift) & 1;
+
+                    if (count == 2 || count == 3) {
+                        grid[i*x_size] |= alive;
+                    } else {
+                        grid[i*x_size] &= dead;
+                    }
+
+                    /* iteration on internal columns */
+                    for (int j=1; j<x_size-1; j++) {
+
+                        count = 0;
+                        for (int b=(i-1)*x_size+j-1; b<(i-1)*x_size+j+2; b++) {
+                            count += (grid[b] >> shift) & 1;
+                        }
+                        count += (grid[i*x_size+j-1] >> shift) & 1;
+                        count += (grid[i*x_size+j+1] >> shift) & 1;
+                        for (int b=(i+1)*x_size+j-1; b<(i+1)*x_size+j+2; b++) {
+                            count += (grid[b] >> shift) & 1;
+                        }
+
+                        if (count == 2 || count == 3) {
+                            grid[i*x_size+j] |= alive;
+                        } else {
+                            grid[i*x_size+j] &= dead;
+                        }
+                    }
+
+                    /* last element of the row */
+                    count = 0;
+                    count += (grid[(i-1)*x_size] >> shift) & 1;
+                    for (int b=i*x_size-2; b<i*x_size+1; b++) {
+                        count += (grid[b] >> shift) & 1;
+                    }
+                    count += (grid[(i+1)*x_size-2] >> shift) & 1;
+                    count += (grid[(i+1)*x_size] >> shift) & 1;
+                    for (int b=(i+2)*x_size-2; b<(i+2)*x_size; b++) {
+                        count += (grid[b] >> shift) & 1;
+                    }
+
+                    if (count == 2 || count == 3) {
+                        grid[(i+1)*x_size-1] |= alive;
+                    } else {
+                        grid[(i+1)*x_size-1] &= dead;
+                    }
+                }
+
+
+                /* updating last row */
+                
+                /* first element of the last row */
+                count = 0;
+                for (int b=(y_size-2)*x_size; b<(y_size-2)*x_size+2; b++) {
+                    count += (grid[b] >> shift) & 1;
+                }
+                count += (grid[n_cells-x_size-1] >> shift) & 1;
+                count += (grid[n_cells-x_size+1] >> shift) & 1;
+                count += (grid[n_cells-1] >> shift) & 1;
+                for (int b=0; b<2; b++) {
+                    count += (grid[b] >> shift) & 1;
+                }
+                count += (grid[x_size-1] >> shift) & 1;
+
+                if (count == 2 || count == 3) {
+                    grid[n_cells-x_size] |= alive;
+                } else {
+                    grid[n_cells-x_size] &= dead;
+                }
+
+                /* internal elements of the last row */
+                for (int j=1; j<x_size-1; j++) {
+
+                    count = 0;
+                    for (int b=j-1; b<j+2; b++) {
+                        count += (grid[n_cells-2*x_size+b] >> shift) & 1;
+                    }
+                    count += (grid[n_cells-x_size+j-1] >> shift) & 1;
+                    count += (grid[n_cells-x_size+j+1] >> shift) & 1;
+                    for (int b=j-1; b<j+2; b++) {
+                        count += (grid[b] >> shift) & 1;
+                    }
+
+                    if (count == 2 || count == 3) {
+                        grid[n_cells-x_size+j] |= alive;
+                    } else {
+                        grid[n_cells-x_size+j] &= dead;
+                    }
+                }
+
+                /* last element of the last row */
+                count = 0;
+                count += (grid[n_cells-2*x_size] >> shift) & 1;
+                for (int b=n_cells-x_size-2; b<n_cells-x_size+1; b++) {
+                    count += (grid[b] >> shift) & 1;
+                }
+                count += (grid[n_cells-2] >> shift) & 1;
+                count += (grid[0] >> shift) & 1;
+                for (int b=x_size-2; b<x_size; b++) {
+                    count += (grid[b] >> shift) & 1;
+                }
+
+                if (count == 2 || count == 3) {
+                    grid[n_cells-1] |= alive;
+                } else {
+                    grid[n_cells-1] &= dead;
+                }
+
+
+                /* saving a snapshot of the system */
+                if (s != 0) {
+
+                    if (gen % s == 0) {
+
+                        sprintf(snap_name, "snapshots/snapshot_%05d.pgm", gen+1);
+                        
+                        write_pgm_image(grid, 1, x_size, y_size, snap_name, gen);
+                    }
+                }
+                
+            }
 
 
 #ifdef TIME 
@@ -660,20 +896,7 @@ int main(int argc, char **argv) {
         free(snap_name);
 
 
-        write_pgm_image(grid, 1, x_size, y_size, "snapshots/final_state.pgm");
-
-
-
-        /////////////// just for test /////////////////
-        ///////////////////////////////////////////////
-        //
-        //for (int i=0; i<y_size; i++) {
-        //    for (int j=0; j<x_size; j++)
-        //        printf("%c ", grid[i*x_size+j]+48);
-        //    printf("\n");
-        //}
-        //
-        ///////////////////////////////////////////////
+        write_pgm_image(grid, 1, x_size, y_size, "snapshots/final_state.pgm", n-1);
 
     }
 
@@ -695,7 +918,7 @@ int main(int argc, char **argv) {
 
 
 /* function to write the status of the system to pgm file */
-void write_pgm_image(BOOL* image, const int maxval, int xsize, int ysize, const char *image_name) {
+void write_pgm_image(BOOL* image, const int maxval, int xsize, int ysize, const char *image_name, int iter) {
 
     FILE* image_file;
     image_file = fopen(image_name, "w");
@@ -703,14 +926,38 @@ void write_pgm_image(BOOL* image, const int maxval, int xsize, int ysize, const 
     /* formatting the header */
     fprintf(image_file, "P5 %d %d\n%d\n", xsize, ysize, maxval);   
 
-    /* writing */
-    fwrite(image, 1, xsize*ysize, image_file); 
-    
-    // same result as previous line
-    //for (int i=0; i<xsize*ysize; i++)
-        //fprintf(image_file, "%c", image[i]); 
+    /* printing first or second bit depending generation */
+    if (iter % 2 == 0) {
+        for (int i=0; i<xsize*ysize; i++)
+            fprintf(image_file, "%c", image[i] >> 1);
+    } else {
+        for (int i=0; i<xsize*ysize; i++)
+            fprintf(image_file, "%c", image[i] & 1);
+    }
 
     fclose(image_file);
+
+
+    /////////////////////////// just for test ///////////////////////////// 
+    //
+    //if (iter % 2 == 0) {
+    //    for (int i=0; i<ysize; i++){
+    //        for (int j=0; j<xsize; j++)
+    //            printf("%c ", (image[i*xsize+j] >> 1)+48);
+    //        printf("\n");
+    //    }
+    //} else {
+    //    for (int i=0; i<ysize; i++) {
+    //        for (int j=0; j<xsize; j++)
+    //            printf("%c ", (image[i*xsize+j] & 1)+48);
+    //        printf("\n");
+    //    }
+    //}
+    //
+    //printf("\n");
+    //
+    //////////////////////////////////////////////////////////////////////
+
 
     return;
 }
