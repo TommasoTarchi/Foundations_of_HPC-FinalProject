efb243036131034a2130c383e8df6fb44f13fc0e
TommasoTarchi <tommaso.tarchi@gmail.com>
2023-04-28 09:46:36 +0200

    test I/O

--- exercise1/src/parallel_gol.c
+++ exercise1/src/parallel_gol.c
@@ -678,7 +678,7 @@ int main(int argc, char **argv) {
                             sprintf(header, "P5 %d %d\n%d\n", x_size, y_size, color_maxval);
 
                             /* writing the header */
-                            access_mode = MPI_MODE_CREATE | MPI_MODE_WRONLY | MPI_MODE_APPEND;
+                            access_mode = MPI_MODE_CREATE | MPI_MODE_WRONLY;
                             check += MPI_File_open(MPI_COMM_SELF, snap_name, access_mode, MPI_INFO_NULL, &f_handle);
                             check += MPI_File_write_at(f_handle, 0, header, header_size, MPI_CHAR, &status);
                             check += MPI_File_close(&f_handle);
