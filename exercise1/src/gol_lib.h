dac3cf9699b99890bb78804628602fe4b0bbe148
TommasoTarchi <tommaso.tarchi@gmail.com>
2023-05-01 16:58:40 +0200

    test

--- exercise1/src/gol_lib.h
+++ exercise1/src/gol_lib.h
@@ -1,6 +1,6 @@
 #define BOOL char
 
 
-int read_pgm_header(unsigned int* head, const char* fname);
+int read_pgm_header(unsigned int*, const char*);
 
 void ordered_evo(int, int, BOOL*, int, const int, const int, const int, const int, const int, const int, int, int, int, int, const int, const int, const int, int, int, MPI_Status*, const int, const int, const int, const int, const int);
