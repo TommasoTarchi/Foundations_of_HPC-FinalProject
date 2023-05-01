5686ad5546c4b9f5ce818116a9f564b1988e74f4
TommasoTarchi <tommaso.tarchi@gmail.com>
2023-05-01 19:39:57 +0200

    fixed job.sh and Makefile

--- exercise1/EPYC/openMP_scal/job.sh
+++ exercise1/EPYC/openMP_scal/job.sh
@@ -22,11 +22,10 @@ export OMP_PROC_BIND=close   # PROVARE ANCHE SPREAD (MAGARI FUNZIONA MEGLIO PER 
 
 echo COMPILING EXECUTABLES...
 echo
-cd ../..
 datafolder=$(pwd)
-srun -n 1 make clean_images
-srun -n 1 make parallel_gol data_folder=datafolder
-cd datafolder
+cd ../..
+make parallel_gol data_folder=$datafolder
+cd $datafolder
 
 
 echo
@@ -38,43 +37,47 @@ echo
 ### setting variables for executables and csv file
 node=EPYC
 scal=openMP
-mat_x_size=20000
-mat_y_size=20000
-n_gen=50
+mat_x_size=10000
+mat_y_size=10000
+n_gen=10
 procs=2
 
 
 echo CREATING/OVERWRITING CSV FILE...
 echo
-echo "#,,," > data.csv
-echo "#node:,${node},," >> data.csv
-echo "#scalability:,${scal},," >> data.csv
-echo "#performance_measure:,time(s),," >> data.csv
-echo "#,,," >> data.csv
-echo "#playground_x_size:,${mat_x_size},," >> data.csv
-echo "#playground_y_size:,${mat_y_size},," >> data.csv
-echo "#generations:,${n_gen},," >> data.csv
-echo "#,,," >> data.csv
-echo "#,,," >> data.csv
-echo "threads,ordered,static,static_in_place" >> data.csv
+datafile=$datafolder/data.csv
+echo "#,,," > ${datafile}
+echo "#node:,${node},," >> $datafile
+echo "#scalability:,${scal},," >> $datafile
+echo "#performance_measure:,time(s),," >> $datafile
+echo "#,,," >> $datafile
+echo "#playground_x_size:,${mat_x_size},," >> $datafile
+echo "#playground_y_size:,${mat_y_size},," >> $datafile
+echo "#generations:,${n_gen},," >> $datafile
+echo "#,,," >> $datafile
+echo "#,,," >> $datafile
+echo "threads,ordered,static,static_in_place" >> $datafile
 
 
 echo PERFORMING MEASURES...
 echo
-for n_threads in $(seq 1 1 64)
+for count in $(seq 1 1 11)
 do
-    for count in $(seq 1 1 10)
-    do 
-        ### generating random playgrounds
-        export OMP_NUM_THREADS=64
-        mpirun -np $procs --map-by socket parallel_gol.x -i -m $mat_x_size -k $mat_y_size
 
-        ### running the evolution
+    ### generating random playground
+    export OMP_NUM_THREADS=20
+    mpirun -np 5 --map-by socket parallel_gol.x -i -m $mat_x_size -k $mat_y_size
+
+    for n_threads in $(seq 1 1 64)
+    do 
+
+	### running the evolution
         export OMP_NUM_THREADS=$n_threads
-        echo -n "${n_threads}" >> data.csv
+        echo -n "${n_threads}" >> $datafile
         mpirun -np $procs --map-by socket parallel_gol.x -r -e 0 -n $n_gen -s 0
         mpirun -np $procs --map-by socket parallel_gol.x -r -e 1 -n $n_gen -s 0
         mpirun -np $procs --map-by socket parallel_gol.x -r -e 2 -n $n_gen -s 0
+	echo >> $datafile
         
         echo
         echo -----------
@@ -91,6 +94,6 @@ echo
 
 echo RELEASING LOADED MODULES AND CLEANING FROM EXECUTABLES AND IMAGES...
 cd ../..
-make clean data=datafolder
+make clean data=$datafolder
 Module purge
-cd datafolder
+cd $datafolder
