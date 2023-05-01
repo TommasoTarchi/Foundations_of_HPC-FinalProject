5aeb1407d20ca215de91463463352565a73eec5d
TommasoTarchi <tommaso.tarchi@gmail.com>
2023-05-01 16:04:47 +0200

    bug fix

--- exercise1/src/gol_lib.h
+++ exercise1/src/gol_lib.h
@@ -1,1 +1,3 @@
+#define BOOL char
+
 void ordered_evo(int, int, BOOL*, int, const int, const int, const int, const int, int, int, const int, const int, const int, int, int, MPI_Status, const int, const int, const int, const int);
